7e9b9e08796ad2f832b5fd2b9ea6f2a55835be25
ggreg1987 <gabriel.gregorio1987@gmail.com>
2020-12-20 12:23:52 -0300

    criando variavel para cada bloco

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let blocos = document.querySelectorAll(".box");
+let bloco1 = document.getElementById('bloco1');
+let bloco2 = document.getElementById('bloco2');
+let bloco3 = document.getElementById('bloco3');
+let bloco4 = document.getElementById('bloco4');
+let bloco5 = document.getElementById('bloco5');
+let bloco6 = document.getElementById('bloco6');
+let bloco7 = document.getElementById('bloco7');
+let bloco8 = document.getElementById('bloco8');
+let bloco9 = document.getElementById('bloco9');
 
 
 let jogador1 = 0;
